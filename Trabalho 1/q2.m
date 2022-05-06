7801395960e7c291c487dcb094d7f041a62f6b32
Lucas Paiva da Silva <lucaspaiva.luc@gmail.com>
2022-05-06 15:24:36 -0300

    FUNCIONA!

--- Trabalho 1/q2.m
+++ Trabalho 1/q2.m
@@ -1,7 +1,7 @@
 # Determinar raiz de f(x)=x.ln(x)-1=0
 format long
 # format short
-f = @(x) (x-1).*tan(x)/(2.*x.*x-x-2)
+f = @(x) (x-1).*tan(x)./(2.*x.*x.-x.-2)
 A = -4; B = 4; # Domínio de busca das raízes
 
 x = metodoDeNewton(A,B,f)
