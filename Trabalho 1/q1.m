4ed813d8d31e6173f318a773697966fc81bf7056
Lucas Paiva da Silva <lucaspaiva.luc@gmail.com>
2022-05-06 11:42:27 -0300

    Adiciona arquivos do trabalho 1

--- Trabalho 1/q1.m
+++ Trabalho 1/q1.m
@@ -1,6 +1,6 @@
 format long
-xx=-60.06      #double
-x=single(xx)   #float/single
+xx=-60.06;      #double
+x=single(xx);   #float/single
 printf("\n xx = %.25f\n",xx)
 printf("\n x  = %.25f\n",x)
 err = ((double(x)-xx)/xx)*100;
